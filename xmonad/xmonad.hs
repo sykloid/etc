076e8cd489580c3513bea574951f196103d0af2e
P.C. Shyamshankar <sykora@lucentbeing.com>
2009-10-19 17:24:50 +0530

    xmonad: Added mouse bindings.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -7,6 +7,7 @@ import System.Exit
 import qualified Data.Map as M
 
 import XMonad
+import XMonad.Actions.CycleWS
 
 import qualified XMonad.StackSet as W
 
@@ -42,7 +43,6 @@ myKeys config@(XConfig {XMonad.modMask = m}) = M.fromList $
         -- XMonad Control
         ((m, xK_q), restart "xmonad" True), -- Restart XMonad.
         ((m .|. shiftMask, xK_F12), io (exitWith ExitSuccess)), -- Quit XMonad.
-
     ]
     ++
 
@@ -53,12 +53,23 @@ myKeys config@(XConfig {XMonad.modMask = m}) = M.fromList $
         | (workspace, numberKey) <- zip (XMonad.workspaces config) [xK_1 .. xK_9]
         , (shiftMask', windowAction) <- [(0, W.greedyView), (shiftMask, W.shift)]]
 
+myMouseBindings (XConfig {XMonad.modMask = m}) = M.fromList $
+
+    [
+        ((m, button1), (\w -> focus w >> mouseMoveWindow w)), -- Float and move while dragging.
+        ((m, button2), (\w -> focus w >> windows W.swapMaster)), -- Raise window to top of stack.
+        ((m, button3), (\w -> focus w >> mouseResizeWindow w)), -- Float and resize while dragging.
+        ((m, button4), (\_ -> prevWS)), -- Switch to previous workspace.
+        ((m, button5), (\_ -> nextWS)), -- Switch to next workspace.
+    ]
+
 -- Amalgamation of settings.
 
 myConfig = defaultConfig {
-    terminal = myTerminal,
-    modMask  = myModMask,
-    keys     = myKeys
+    terminal      = myTerminal,
+    modMask       = myModMask,
+    keys          = myKeys,
+    mouseBindings = myMouseBindings,
 }
 
 -- Run it.
