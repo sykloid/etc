d179f367c64cf6558ec11666b740ac2599a01f23
P.C. Shyamshankar <sykora@lucentbeing.com>
2009-10-19 16:49:14 +0530

    xmonad: Added basic keys, and modMask.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -10,9 +10,43 @@ import XMonad
 
 import qualified XMonad.StackSet as W
 
+-- Keys
+
+myModMask = mod4Mask -- The Windows Key, aka "Super"
+
+myKeys config@(XConfig {XMonad.modMask = m}) = M.fromList $
+
+    [
+        -- The Terminal
+        ((m, xK_c), spawn $ XMonad.terminal config), -- Start a new terminal.
+
+        -- Window Navigation
+        ((m, xK_t), windows W.focusDown), -- Focus next window.
+        ((m, xK_s), windows W.focusUp), -- Focus previous window.
+
+        -- Window Management
+        ((m, xK_x), kill), -- Kill the window.
+
+        ((m .|. shiftMask, xK_t), windows W.swapDown), -- Swap with next.
+        ((m .|. shiftMask, xK_s), windows W.swapUp), -- Swap with previous.
+
+        -- Layout Management
+        ((m, xK_space), sendMessage NextLayout), -- Rotate to next layout.
+        ((m, xK_comma), sendMessage (IncMasterN 1)), -- Increment number of master windows.
+        ((m, xK_period), sendMessage (IncMasterN (-1)), -- Decrement number of master windows.
+
+        -- XMonad Control
+        ((m, xK_q), restart "xmonad" True), -- Restart XMonad.
+        ((m .|. shiftMask, xK_F12), io (exitWith ExitSuccess)), -- Quit XMonad.
+
+    ]
+
 -- Amalgamation of settings.
 
-myConfig = defaultConfig
+myConfig = defaultConfig {
+    modMask = myModMask,
+    keys    = myKeys
+}
 
 -- Run it.
 
