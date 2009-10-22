8560d74e0ab8ba4a5a3d778263dd8824956f173e
P.C. Shyamshankar <sykora@lucentbeing.com>
2009-10-22 17:54:31 +0530

    zsh: Setup some basic aliases.

---
+++ zsh/aliases.zsh
@@ -0,0 +1,11 @@
+# aliases.zsh: Sets up aliases which make working at the command line easier.
+# P.C. Shyamshankar <sykora@lucentbeing.com>
+
+# Looking around, and moving about.
+alias ls="ls --color=tty"
+alias l="ls -lhB --color=tty"
+
+alias ...=../..
+
+# Some application shortcuts.
+alias m=mplayer
