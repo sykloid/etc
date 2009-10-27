e0f6ba8a3f354c9c50ea072c1530400cea96af4c
P.C. Shyamshankar <sykora@lucentbeing.com>
2009-10-27 17:54:57 +0530

    zsh: Initial 256 color prompt.

--- zsh/prompt_256.zsh
+++ zsh/prompt_256.zsh
@@ -1,4 +1,19 @@
-# prompt.zsh: A custom prompt for zsh.
+# prompt.zsh: A custom prompt for zsh (256 color version).
 # P.C. Shyamshankar <sykora@lucentbeing.com>
 
-PROMPT="%n@%m: %1~> "
+local p="%{$FX[reset]$FG[243]%}"
+
+local name="%{$FX[reset]$FG[114]%}%n"
+local host="%{$FX[reset]$FG[117]%}%m"
+local jobs="%1(j.(%{$FX[reset]$FG[197]%}%j job%2(j.s.)${p})-.)"
+local time="%{$FX[reset]$FG[215]%}%*"
+local dir="%{$FX[reset]$FG[105]%}%~"
+
+local last="%(?..%{$FX[reset]$FG[203]%}%??${p}:)"
+local hist="%{$FX[reset]$FG[220]%}%!!"
+local priv="%{$FX[reset]$FG[245]%}%#"
+
+git_branch=""
+
+PROMPT="${p}(${name}${p}@${host}${p})-${jobs}(${time}${p})-(${dir}${p}\${git_branch}${p})
+(${last}${p}${hist}${p}:${priv}${p})- %{$FX[reset]%}"
