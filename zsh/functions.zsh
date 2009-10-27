79ddb675c0494b737f902fd59b679169346ad631
P.C. Shyamshankar <sykora@lucentbeing.com>
2009-10-27 17:52:15 +0530

    zsh: Added helper function to get current git branch.

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,3 +1,16 @@
+# functions.zsh: Custom functions, and function invocations.
+# P.C. Shyamshankar <sykora@lucentbeing.com>
+
+if (( C == 256 )); then
+    autoload spectrum && spectrum # Set up 256 color support.
+fi
+
+# Simple function to get the current git branch.
+function git_current_branch() {
+    ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+    print "${ref#refs/heads/}"
+}
+
 case $TERM in
     *xterm*|*rxvt*)
         # Special function precmd, executed before displaying each prompt.
