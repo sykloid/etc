6667b5ab93dfbaf24c8055938ec90a4a2ae842ab
P.C. Shyamshankar <sykora@lucentbeing.com>
2014-07-18 20:30:43 -0400

    emacs: Initial init.el, with lazy-loading scaffolding.

---
+++ emacs/init.el
@@ -0,0 +1,36 @@
+;;; init -- Custom Emacs Configuration.
+;;; P.C. Shyamshankar "sykora" <sykora@lucentbeing.com>
+
+;;; Commentary:
+
+;;; This configuration method is inspired by MilkyPostman's single-file setup at
+;;; `http://milkbox.net/note/single-file-master-emacs-configuration/'. It makes
+;;; use of the same `after' macro, tweaked slightly to accommodate multiple load
+;;; targets.
+
+;;; I also take inspiration from an extension of the above approach described at
+;;; `http://nullprogram.com/blog/2013/06/02/'. This method permits dependencies
+;;; on more than one package, but also goes ahead and installs all packages.
+;;; While I still fundamentally want the ability to install all missing packages
+;;; mentioned in the configuration, I'd rather not have that happen without my
+;;; explicit authorization.
+
+;;; Code:
+
+;;;; Scaffolding
+
+(defmacro eval-after-load-all (features form)
+  "Arrange that if, and only if, all `FEATURES' are loaded, `FORM' is evaluated."
+  (declare (indent defun))
+  (if (null (cdr features))
+      `(eval-after-load ,(car features) ,form)
+    `(eval-after-load ,(car features) (eval-after-load-all ,(cdr features) ,form))))
+
+(defmacro after (features &rest body)
+  "Arrange that if, and only if, all `FEATURES' are loaded, `BODY' is evaluated.
+Additionally, `BODY' is wrapped in a lambda so that it is properly byte-compiled."
+  (declare (indent defun))
+  `(eval-after-load-all ,features ((lambda () (quote (progn ,@body))))))
+
+(provide 'init)
+;;; init.el ends here
