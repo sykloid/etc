bde92064c91eac5dbe0338239e267cff8eea7c15
P.C. Shyamshankar <sykora@lucentbeing.com>
2014-08-01 14:00:53 -0400

    emacs: Add missing comment-dwim-toggle.el.

---
+++ emacs/lisp/comment-dwim-toggle.el
@@ -0,0 +1,19 @@
+;;; comment-dwim-toggle.el - Custom Comment Functionality
+;;; P.C. Shyamshankar "sykora" <sykora@lucentbeing.com>
+
+;;; Commentary:
+
+;;; This function provides a convenient method to context dependently comment/uncomment.
+
+(defun comment-dwim-toggle (&optional arg)
+  "Context dependently toggle comment/uncomment.
+If a region is active, comment/uncomment it. Otherwise,
+comment/uncomment the current line."
+  (interactive "*P")
+  (comment-normalize-vars)
+  (if (not (region-active-p))
+      (comment-or-uncomment-region (line-beginning-position) (line-end-position))
+    (comment-dwim arg)))
+
+(provide 'comment-dwim-toggle)
+;;; comment-dwim-toggle.el ends here
