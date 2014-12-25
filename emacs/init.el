1a3293098bc26611141ea136daa7061c1d99a887
P.C. Shyamshankar <sykora@lucentbeing.com>
2014-12-25 09:56:48 +0530

    emacs: Disable logging on done/clock-out.
    Having org-mode prompt for a log message on every clock-out/state-change
    is too time-consuming; a saner alternative would be to have the prefix
    argument to any of these commands prompt instead, leaving the default
    version unchanged.

--- emacs/init.el
+++ emacs/init.el
@@ -623,8 +623,6 @@ Additionally, `BODY' is wrapped in a lambda so that it is properly byte-compiled
                         ("\\.pdf::\\([0-9]+\\)" . "okular --page %1 --unique %s")
                         ("\\.pdf" . "okular --unique %s")))
 
-  (set 'org-log-done 'note)
-  (set 'org-log-note-clock-out t)
   (set 'org-log-into-drawer "LOG")
 
   (set 'org-refile-use-outline-path 'file)
