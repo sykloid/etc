dd916b7efa5f1db27dab3c0109627310cfffec54
P.C. Shyamshankar <sykora@lucentbeing.com>
2015-12-29 14:57:14 -0500

    emacs: Add use-package bootstrap.
    This is the beginning of the 4th rewrite of my emacs config.
    
    This version assumes that a full network connection exists (how else
    would you clone the git repository?), and that all packages are to be
    installed and configured automatically on startup.
    
    The previous philosophy of a perfectly gracefully degrading
    configuration was too hard to adhere to; there were too many possible
    error scenarios to account for.

---
+++ emacs/init-use-package.el
@@ -0,0 +1,20 @@
+;; Emacs Initialization
+;; P.C.Shyamshankar 'sykora' <sykora@lucentbeing.com>
+
+;; This is rewrite #4.
+
+;; Package System Initialization -- Must be done ahead of time.
+(require 'package)
+
+;; I gave up on melpa-stable, it's anything but.
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+;; Bootstrap use-package itself, if absent.
+(unless (require 'use-package nil 'silent)
+  (package-refresh-contents)
+  (package-install 'use-package)
+  (require 'use-package))
