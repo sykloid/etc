e7fe44b4054028e5023b3d201555cc4e359bf489
P.C. Shyamshankar <sykora@lucentbeing.com>
2015-12-30 12:25:52 -0500

    emacs: Port over evil-mode configuration.
    A call to define-prefix-command in the :config section creates not only
    a keymap, but also a function that can be used in the action section of
    a :bind clause.
    
    Evil defines its keymaps directly as variables; the corresponding
    function is not created, and therefore cannot be bound through :bind.

--- emacs/init-use-package.el
+++ emacs/init-use-package.el
@@ -28,3 +28,89 @@
 (use-package undo-tree
   :diminish undo-tree-mode)
 
+(use-package evil
+  :ensure t
+  :bind (:map evil-normal-state-map
+              ("n" . evil-backward-char)
+              ("e" . evil-next-visual-line)
+              ("i" . evil-previous-visual-line)
+              ("o" . evil-forward-char)
+
+              ("h" . evil-insert-state)
+              ("H" . evil-insert-line)
+              ("a" . evil-append)
+              ("A" . evil-append-line)
+              ("y" . evil-open-below)
+              ("Y" . evil-open-above)
+
+              ("t" . evil-search-next)
+              ("T" . evil-search-previous)
+
+              ("k" . evil-yank)
+              ("m" . evil-paste-after)
+              ("M" . evil-paste-before)
+
+              ("q" . nil)
+              ("Q" . evil-record-macro)
+
+              ("s" . evil-utility-map))
+
+  :bind (:map evil-motion-state-map
+              ("n" . evil-backward-char)
+              ("e" . evil-next-visual-line)
+              ("i" . evil-previous-visual-line)
+              ("o" . evil-forward-char)
+
+              ("t" . evil-search-next)
+              ("T" . evil-search-previous))
+
+  :bind (:map evil-visual-state-map
+              ("n" . evil-backward-char)
+              ("e" . evil-next-visual-line)
+              ("i" . evil-previous-visual-line)
+              ("o" . evil-forward-char)
+
+              ("k" . evil-yank)
+              ("m" . evil-paste-after)
+              ("M" . evil-paste-before)
+
+              ("y" . evil-visual-exchange-corners)
+
+              ("h" . evil-inner-text-objects-map))
+
+  :bind (:map evil-window-map
+              ("n" . evil-window-left)
+              ("e" . evil-window-down)
+              ("i" . evil-window-up)
+              ("o" . evil-window-right)
+
+              ("C-n" . evil-window-left)
+              ("C-e" . evil-window-down)
+              ("C-i" . evil-window-up)
+              ("C-o" . evil-window-right)
+
+              ("q" . evil-window-delete)
+              ("d" . delete-other-windows)
+              ("D" . delete-other-windows-vertically)
+
+              ("u" . winner-undo)
+              ("U" . winner-redo))
+
+  :bind (:map evil-utility-map
+              ("v" . evil-visual-restore)
+              ("m" . evil-mark-last-yank))
+
+  :init
+  (evil-mode t)
+
+  (defun evil-mark-last-yank ()
+    (interactive)
+    (evil-visual-make-selection (evil-get-marker ?[) (evil-get-marker ?])))
+
+  :config
+  (setc evil-move-beyond-eol t)
+  (setc evil-want-fine-undo nil)
+
+  (define-prefix-command 'evil-utility-map)
+
+  (define-key evil-operator-state-map "h" evil-inner-text-objects-map))
