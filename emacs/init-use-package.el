8cb79f5d2868a5528f4d07882cb67d53001cbc83
P.C. Shyamshankar <sykora@lucentbeing.com>
2016-01-01 16:08:28 -0500

    emacs: Add theme-loader hooks.

--- emacs/init-use-package.el
+++ emacs/init-use-package.el
@@ -40,6 +40,13 @@
 
 (setc inhibit-splash-screen t)
 
+(if (daemonp)
+    (add-hook 'after-make-frame-functions
+              (lambda (frame)
+                (with-selected-frame frame
+                  (load-theme 'skywave-gui))))
+  (load-theme 'skywave-gui))
+
 (use-package winner
   :init (winner-mode t))
 
