12ea52d3f190deeaa26cc31becc439d69eaadcaf
P.C. Shyamshankar <sykora@lucentbeing.com>
2017-07-01 14:23:17 -0400

    emacs: Add utility set/hook forms.

--- emacs/init-5.el
+++ emacs/init-5.el
@@ -11,3 +11,32 @@
 
 (setq package-enable-at-startup nil)
 (eval-and-compile (package-initialize))
+
+;; ** Utility Forms
+;; *** Set Forms
+;; The customization system is great for option discovery, but lousy for programmatic configuration.
+;; Nevertheless, it has become the de-facto standard for specifying user-facing options, particuarly
+;; using custom set logic. It's impossible to escape.
+(defalias 'setl 'setq "Set a variable (buffer) locally.")
+(defmacro setg (variable value)
+  "Set a variable globally."
+  `(customize-set-variable ',variable ,value))
+
+;; *** Hook Forms
+;; This macro solves multiple problems:
+;;   1. It removes the need to remember `add-hook' syntax.
+;;   2. It permits (but does not require) naming a hook entry, for removal afterwards. If a name is
+;;      not given, one will be generated based on the hook being added to.
+;;   3. It allows adding to hooks of functions which take arguments.
+(defmacro add-hook+ (hook-spec arg-spec &rest body)
+  "Multi-purpose hook handler."
+  (declare (indent 2))
+  (let* ((separator "/:")
+         (components (split-string (symbol-name hook-spec) separator))
+         (hook (if (= 1 (length components))
+                   (car components)
+                 (mapconcat #'identity (butlast components) separator)))
+         (name (if (= 1 (length components))
+                   (symbol-name (cl-gensym (concat hook separator)))
+                 (car (last components)))))
+    `(add-hook (intern ,hook) (defalias (intern ,name) (lambda ,arg-spec ,@body)) t)))
