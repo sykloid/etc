5f71d583c3eb3d89e7398c95754ef967738743b0
P.C. Shyamshankar <sykora@lucentbeing.com>
2017-07-26 07:48:57 -0400

    emacs: Switch org-italic faces around.

--- emacs/lisp/themes/skywave-theme.el
+++ emacs/lisp/themes/skywave-theme.el
@@ -111,7 +111,7 @@
 
  ;; Org
  '(org-bold ((t (:foreground "#EA7EAF" :weight ultra-bold))))
- '(org-italic ((t (:foreground "#7EEABF" :slant italic))))
+ '(org-italic ((t (:foreground "#9ED9FF" :slant italic))))
  '(org-underline ((t (:underline t))))
  '(org-verbatim ((t (:foreground "#F0E9A1"))))
  '(org-code ((t (:foreground "#69ACC8"))))
