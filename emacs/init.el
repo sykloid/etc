d6b8a53b4f691d3827196cd01d6cbdd2d9d62b1e
P.C. Shyamshankar <shyam@sykloid.org>
2020-04-29 22:46:00 +0000

    emacs: Add-in evil-mode configuration.

--- emacs/init.el
+++ emacs/init.el
@@ -50,6 +50,52 @@
       "S-TAB" (general-predicate-dispatch nil
                 outshine-mode 'outshine-cycle-buffer))))
 
+;; * Evil
+(use-package evil
+  :init
+  (evil-mode 1)
+
+  :config
+  (general-setq evil-move-beyond-eol t)
+  (general-setq evil-split-window-below t)
+  (general-setq evil-vsplit-window-right t)
+  :general
+  (:states 'normal
+   "a" 'evil-append
+   "A" 'evil-append-line
+   "h" 'evil-insert-state
+   "H" 'evil-insert-line
+   "y" 'evil-open-below
+   "Y" 'evil-open-above
+   "Q" 'evil-record-macro)
+
+  (:states '(motion normal visual)
+   "n" 'evil-backward-char
+   "e" 'evil-next-visual-line
+   "i" 'evil-previous-visual-line
+   "o" 'evil-forward-char
+
+   "N" 'evil-beginning-of-line
+   "O" 'evil-end-of-line)
+
+  (:states '(normal visual)
+   "m" 'evil-paste-after
+   "M" 'evil-paste-before)
+
+  (:states '(operator visual)
+   "h" '(:keymap evil-inner-text-objects-map))
+
+  (:keymaps 'evil-normal-state-map "q" nil)
+
+  (:keymaps 'evil-window-map
+   "n" 'evil-window-left
+   "e" 'evil-window-down
+   "i" 'evil-window-up
+   "o" 'evil-window-right
+
+   "d" 'delete-other-windows
+   "q" 'evil-window-delete))
+
 ;; * Minor Modes
 (use-package outshine
   :init
