9f496e7153f3e61dce0d8e896f59370ec5835526
P.C. Shyamshankar <shyam@sykloid.org>
2020-05-05 22:35:29 +0000

    emacs: Add in basic skywave-theme definitions.

--- emacs/skywave-theme.el
+++ emacs/skywave-theme.el
@@ -7,4 +7,27 @@
 
 (deftheme skywave "The Skywave Emacs Theme")
 
+(custom-theme-set-faces
+ 'skywave
+ '(font-lock-builtin-face ((t :foreground "#56AEE8")))
+ '(font-lock-comment-face ((t :foreground "#666666")))
+ '(font-lock-constant-face ((t :foreground "#C45271")))
+ '(font-lock-doc-face ((t :foreground "#FFA8BD")))
+ '(font-lock-function-name-face ((t :foreground "#F5F3B8")))
+ '(font-lock-keyword-face ((t :foreground "#36B9C2")))
+ '(font-lock-preprocessor-face ((t :foreground "#F0355A")))
+ '(font-lock-string-face ((t :foreground "#CEA3F7")))
+ '(font-lock-type-face ((t :foreground "#FAC78C")))
+ '(font-lock-variable-name-face ((t :foreground "#4ED998")))
+
+ '(outline-1 ((t (:foreground "#FAC78C"))))
+ '(outline-2 ((t (:foreground "#6EB8F5"))))
+ '(outline-3 ((t (:foreground "#C45271"))))
+ '(outline-4 ((t (:foreground "#4ED998"))))
+
+ '(region ((t (:background "#444444"))))
+
+ '(isearch ((t (:foreground "#FE0012" :background "#DFDFDF"))))
+ '(lazy-highlight ((t (:background "#777777")))))
+
 (provide-theme 'skywave)
