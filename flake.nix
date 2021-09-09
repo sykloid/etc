cab5b2a19511a4df882101ee228359a8c78ee5bb
P.C. Shyamshankar <shyam@sykloid.org>
2021-09-09 21:26:54 +0000

    nix: Switch to nix flakes.
    Also updates emacs to 27, with package changes.

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "Airlift";
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/master;
+
+  outputs = { self, nixpkgs }: {
+
+    defaultPackage.x86_64-linux = with import nixpkgs { system = "x86_64-linux"; }; buildEnv {
+      name = "airlift";
+      paths = [
+          ansible
+          bat
+          fd
+          git
+          just
+          less
+          man
+          nixUnstable
+          ripgrep
+          tmux
+          util-linux
+          zsh
+
+          ((emacsPackagesFor emacs27-nox).emacsWithPackages
+            (epkgs: with epkgs.melpaPackages; [
+              evil-commentary
+              company
+              diminish
+              evil
+              evil-surround
+              general
+              helm
+              helm-projectile
+              helm-rg
+              magit
+              mwim
+              no-littering
+              outshine
+              projectile
+              use-package
+              which-key
+
+              dockerfile-mode
+              just-mode
+              markdown-mode
+              nix-mode
+              rust-mode
+              yaml-mode
+            ])
+          )
+      ];
+    };
+  };
+}
