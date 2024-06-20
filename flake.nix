5f27f82d0188bbbc2fe9ce14490201da5889d132
P.C. Shyamshankar <shyam@sykloid.org>
2024-06-20 09:42:52 +0530

    [nix] Refactor flake to use home-manager.
    This rather impactful refactor allows the use of home-manager to
    handle package and file provisioning. This has several benefits:
    
    1. Having both filse and packages provisioned at the same time means
       they won't be out-of-sync because someone forgot to run `just`.
    2. So far, we appear to be able to provision on MacOS just as well as
       in Linux and in a container.

--- flake.nix
+++ flake.nix
@@ -1,11 +1,35 @@
 {
   description = "Airlift";
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-23.05;
 
-  outputs = { self, nixpkgs }:
-    let output = system: with import nixpkgs { system = system; }; buildEnv {
-      name = "airlift";
-      paths = [
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      darwin = { config, pkgs, ... }: {
+        home.enableNixpkgsReleaseCheck = false;
+        home.username = "sykloid";
+        home.homeDirectory = "/Users/sykloid";
+        home.stateVersion = "24.05";
+        imports = [ definition ];
+      };
+
+      linux = { config, pkgs, ... }: {
+        home.enableNixpkgsReleaseCheck = false;
+        home.username = "sykloid";
+        home.homeDirectory = "/home/sykloid";
+        home.stateVersion = "24.05";
+        imports = [ definition ];
+      };
+
+      definition = {pkgs, config, ...}: {
+        home.packages = with pkgs; [
           bat
           direnv
           docker-client
@@ -15,58 +39,84 @@
           less
           man
           ncurses
-          nixUnstable
           ripgrep
           tmux
           tree
           util-linux
           zsh
 
-          ((emacsPackagesFor emacs29-nox).emacsWithPackages
-            (epkgs: with epkgs.melpaPackages; [
-              backline
-              bicycle
-              company
-              diminish
-              direnv
-              epkgs.rainbow-mode
-              evil
-              evil-args
-              evil-commentary
-              evil-exchange
-              evil-surround
-              evil-surround
-              expand-region
-              forge
-              general
-              magit
-              magit-todos
-              mwim
-              no-littering
-              outline-minor-faces
-              undo-fu
-              use-package
-              which-key
+          rye
+        ];
 
-              consult
-              embark
-              epkgs.embark-consult
-              epkgs.orderless
-              epkgs.vertico
-              marginalia
+        programs.emacs = {
+          enable = true;
+          package = pkgs.emacs-nox;
+          extraPackages = epkgs: with epkgs; [
+            backline
+            bicycle
+            breadcrumb
+            company
+            diminish
+            direnv
+            epkgs.rainbow-mode
+            evil
+            evil-args
+            evil-commentary
+            evil-exchange
+            evil-surround
+            evil-surround
+            expand-region
+            forge
+            general
+            magit
+            magit-todos
+            mwim
+            no-littering
+            outline-minor-faces
+            undo-fu
+            use-package
+            which-key
 
-              dockerfile-mode
-              haskell-mode
-              just-mode
-              markdown-mode
-              nix-mode
-              rust-mode
-              yaml-mode
-            ])
-          )
-      ];
-    }; in {
-      defaultPackage.aarch64-linux = output "aarch64-linux";
-      defaultPackage.x86_64-linux = output "x86_64-linux";
+            consult
+            embark
+            epkgs.embark-consult
+            epkgs.orderless
+            epkgs.vertico
+            marginalia
+
+            dockerfile-mode
+            haskell-mode
+            just-mode
+            markdown-mode
+            nix-mode
+            rust-mode
+            yaml-mode
+          ];
+        };
+
+        home.file = {
+          ".emacs.d/init.el".source = ./emacs/init.el;
+          ".emacs.d/skywave-theme.el".source = ./emacs/skywave-theme.el;
+
+          ".tmux.conf".source = ./tmux/tmux.conf;
+
+          ".zprofile".source = ./zsh/zprofile;
+          ".zshrc".source = ./zsh/zshrc;
+        };
+
+        home.sessionVariables = { };
+
+        programs.home-manager.enable = true;
+      };
+    in {
+      homeConfigurations."darwin" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages."aarch64-darwin";
+        modules = [ darwin ];
+      };
+
+      homeConfigurations."linux" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages."aarch64-linux";
+        modules = [ linux ];
+      };
     };
 }
